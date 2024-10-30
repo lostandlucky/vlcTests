96a29bae3d23bf31791c27dafb0c8d6a8c0fad78
Steve Day <stevenrday@gmail.com>
2024-10-30 16:39:24 -0400

    Autostart on startup shell script

---
+++ start_mainloop.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Start the matchbox window manager
+# Print Application starting
+echo "########"
+echo "########"
+echo "Starting Application"
+echo "########"
+echo "########"
+
+#export DISPLAY=:0 
+cd /home/IntExp/Documents/vlcWork
+
+# Xvfb :99 -screen 0 1024x768x16 &
+
+# # # Wait for Xvfb to start
+# sleep 2
+
+# # Export the DISPLAY environment variable
+# export DISPLAY=:99
+
+# matchbox-window-manager &
+
+# Run the Python script
+sudo -E /home/IntExp/Documents/vlcWork/.venv/bin/python /home/IntExp/Documents/vlcWork/MainLoop.py
+
+exit 0
